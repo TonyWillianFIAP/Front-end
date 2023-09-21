23885f4147b1d999535929f1d176e1b1bc8e42bd
TonyWillianFIAP <rm550667@fiap.com.br>
2023-09-20 22:25:54 -0300

    Estrutura de rotas

---
+++ src/Feminino.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Feminino() {
+  return (
+    <div>Feminino</div>
+  )
+}
+
+export default Feminino
---
+++ src/Masculino.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Masculino = () => {
+  return (
+    <div>Masculino</div>
+  )
+}
+
+export default Masculino
