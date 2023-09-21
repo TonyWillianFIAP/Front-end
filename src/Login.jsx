b20b1d2db4c8c269cbc51f760c8ca6069f42bbbe
TonyWillianFIAP <rm550667@fiap.com.br>
2023-09-20 22:46:43 -0300

    Commit final

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Endereço de e-mail</Form.Label>
+        <Form.Control type="email" placeholder="Insira seu email" />
+
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Senha</Form.Label>
+        <Form.Control type="password" placeholder="Insira a senha" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Continuar
+      </Button>
+      <br/>
+      <Form.Text className="text-muted">
+          Nós nunca vamos compartilhar suas informações.
+        </Form.Text>
+    </Form>
+  );
 }
 
 export default Login
